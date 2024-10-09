e50d876bbd142b32dd186cc9f93169553daf93f1
Maulana Abdul Rahman <maulanaabdulrahman31@gmail.com>
2024-10-09 14:10:24 +0800

    menambahkan input type file

--- app/Views/pasien/chat.php
+++ app/Views/pasien/chat.php
@@ -55,7 +55,7 @@
 
                 <div class="header-right">
                     <div class="notification-box">
-                        <a class="text-white" href="<?=site_url('pasien/list_chat') ?>">
+                        <a class="text-white" href="<?= site_url('pasien/list_chat') ?>">
                             <i class="ri-chat-4-line"></i>
                         </a>
                     </div>
@@ -135,7 +135,12 @@
         <div class="chat-card">
             <form action="" method="post" class="d-flex gap-1 p-2 bg-white pt-1">
                 <input type="text" placeholder="Type a message..." class="chat-input">
-                <button class="border-0 btn-gradient p-1 btn-chat">📷</button>
+                <label for="file-upload" class="border-0 btn-gradient btn-chat p-2 text-center"><svg class="w-50" viewBox="0 0 24 24"
+                        fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M9 3H15L17 5H21C21.5523 5 22 5.44772 22 6V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V6C2 5.44772 2.44772 5 3 5H7L9 3ZM12 19C15.3137 19 18 16.3137 18 13C18 9.68629 15.3137 7 12 7C8.68629 7 6 9.68629 6 13C6 16.3137 8.68629 19 12 19ZM12 17C9.79086 17 8 15.2091 8 13C8 10.7909 9.79086 9 12 9C14.2091 9 16 10.7909 16 13C16 15.2091 14.2091 17 12 17Z" />
+                    </svg></label>
+                    <input id="file-upload" type="file" name="upload-image">
                 <input type="submit" value="Kirim" class="border-0 btn-gradient p-1 btn-chat">
             </form>
         </div>
